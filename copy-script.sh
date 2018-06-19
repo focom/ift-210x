e783e792f3c8fd768e7a1fdda4e004c4a138b61e
Pierre Valentin <pvalentin@localhost.localdomain>
2018-06-19 00:04:36 -0400

    wip

--- copy-script.sh
+++ copy-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo rm -rf /var/named/*
-sudo rm /etc/named/named.con
+sudo rm /etc/named/named.conf
 sudo cp named.conf /etc/named/
 sudo cp -r ./* -t /var/named
 sudo chmod -R 777 /var/named
 sudo chgrp -R named /var/named/
 sudo chgrp -R named /etc/named/
-sudo ./usr/bin/systemctl restart named
+systemctl restart named
+systemctl status named
