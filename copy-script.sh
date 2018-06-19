b763403e16523c5b2930aef92551a61a1becb05c
Pierre Valentin <pvalentin@localhost.localdomain>
2018-06-18 23:32:58 -0400

    wip

--- copy-script.sh
+++ copy-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+su
 rm -rf /var/named/*
 rm /etc/named/named.con
 cp named.conf /etc/named/
 cp -r ./* -t /var/named
+chmod -R 777 /var/named
+chgrp -R named /var/named
+chgrp -R named /etc/named
+
