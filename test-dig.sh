4c6624e3ede03bbbe401c61cc8c398173d449f26
Pierre Valentin <pvalentin@localhost.localdomain>
2018-06-18 23:22:32 -0400

    add test script

---
+++ test-dig.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+dig @127.0.0.1 www.evilcorpca.com
+read lol
+
+
+clear
+dig @127.0.0.1 www.evilcorp.ca
+read l
+
+
+clear
+dig @127.0.0.1 www.secure.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 webmail.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 courriel.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 vpn.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 ns1.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 ns2.evilcorpca.com
+read l
+
+clear
+dig @127.0.0.1 fichiers.evilcorpca.local
+read l
+
+clear
+dig @127.0.0.1 passerelle.evilcorpca.local
+read l
+
+clear
+echo ================== Done ====================
+
